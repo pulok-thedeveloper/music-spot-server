11196a19414c8d07240557e75726570452123874
pulok-thedeveloper <mdpulokhasan@gmail.com>
2022-11-29 22:22:21 +0600

    Admin and seller check done

--- index.js
+++ index.js
@@ -133,6 +133,39 @@ async function run(){
             res.send(users);
           });
 
+          app.get('/users/admin/:email', async(req,res)=>{
+            const email = req.params.email;
+            const query = {email}
+            const user = await userCollection.findOne(query);
+            res.send({isAdmin: user?.role === 'admin'});
+          })
+
+          app.get('/users/seller/:email', async(req,res)=>{
+            const email = req.params.email;
+            const query = {email}
+            const user = await userCollection.findOne(query);
+            res.send({isSeller: user?.role === 'seller'});
+          })
+          
+          app.put('/users/admin/:id', verifyJWT, async(req, res) =>{
+            const decodedEmail = req.decoded.email;
+            const query = {email: decodedEmail};
+            const user = await userCollection.findOne(query);
+            if(user?.role !== 'admin'){
+                return res.status(403).send({message: 'forbidden access'})
+            }
+            const id = req.params.id;
+            const filter = {_id: ObjectId(id)}
+            const options = {upsert: true};
+            const updatedDoc = {
+                $set:{
+                    role: 'admin'
+                }
+            }
+            const result = await userCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+          })
+
         
     }
     finally{
